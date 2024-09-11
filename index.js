65a133c6f799fc2842e620a16bbaf0495ff8ef83
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2024-09-11 15:44:30 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -32,10 +32,19 @@ const criarMeta = () => {}
 function criarMeta() {}*/
 
 function start(){
-    let count = 1
-    while(count <= 10){
-        console.log(count)
-        count = count + 1
+    
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                return
+        }
     }
 }
 
