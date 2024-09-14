1814c56130206caefa414c80926531d489ed42cc
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2024-09-14 16:30:15 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -55,8 +55,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        menssage: "Metas Realizadas",
+        menssage: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async() => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+      console.log("Não existem metas abertas :)")
+      return 
+    }
+
+    await select({
+        message: "Metas abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -80,6 +96,10 @@ const start = async() => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "sair",
                     value: "sair"
                 },
@@ -96,6 +116,9 @@ const start = async() => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
