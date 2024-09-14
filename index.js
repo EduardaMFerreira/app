890f2ec54391436fa5906ac4903c975c8e9d9e76
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2024-09-14 19:04:30 -0300

    remover metas

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const listarMetas = async () => {
 
         meta.checked = true
     })
-    console.log("Meta(s) marcada(s) como consluída(a)")
+    console.log("Meta(s) marcada(s) como conscluída(s)")
 }
 
 const metasRealizadas = async () => {
@@ -55,7 +55,7 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        menssage: "Metas Realizadas" + realizadas.length,
+        menssage: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -74,6 +74,31 @@ const metasAbertas = async() => {
         message: "Metas abertas" + abertas.length,
         choices: [...abertas]
     })
+}
+
+const deletarMetas = async() => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return {value: meta.value, checked: false}
+    })
+
+    const itensADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+
+    if(itensADeletar.length == 0) {
+        console.log("Nenhum item para deletar!")
+        return
+    }
+
+    itensADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    console.log("Meta(s) deletada(s) com sucesso!")
 }
 
 const start = async() => {
@@ -100,6 +125,10 @@ const start = async() => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
                     name: "sair",
                     value: "sair"
                 },
@@ -119,6 +148,9 @@ const start = async() => {
                 break
             case "abertas":
                 await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
                 break
             case "sair":
                 console.log("Até a próxima!")
